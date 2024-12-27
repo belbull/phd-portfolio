ece799cc92f75948212349fa6586909889de9d4a
Beleicia Bullock <beleiciabullock@Beleicias-MacBook-Pro-2.local>
2024-12-26 22:53:26 -0500

    hero: about paragraph

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
-export const GENERATE_SLUG_FROM_TITLE = true
-export const TRANSITION_API = true
+export const SITE_TITLE = "Beleicia Bullock";
+export const SITE_DESCRIPTION =
+  "CS Ph.D. Candidate at Stanford using HCI to craft methods for more equitable design";
+export const GENERATE_SLUG_FROM_TITLE = true;
+export const TRANSITION_API = true;
